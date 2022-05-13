a37855e777d5f882e57170a2f3c8b6540ed33731
EthanHunt2006 <105423632+EthanHunt2006@users.noreply.github.com>
2022-05-13 15:35:59 +0200

    added stuff

---
+++ app/src/main/java/com/example/opscpoe/add_item_page.java
@@ -0,0 +1,18 @@
+package com.example.opscpoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class add_item_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item_page);
+
+
+
+
+    }
+}
