e5c706752c41379fa48d76fb8c6d096101912eaf
EthanHunt2006 <105423632+EthanHunt2006@users.noreply.github.com>
2022-06-20 22:34:57 +0200

    Updates recycle page class

--- app/src/main/java/com/example/opscpoe/Activity/RecyclePage.java
+++ app/src/main/java/com/example/opscpoe/Activity/RecyclePage.java
@@ -26,7 +26,7 @@ import java.util.Map;
 
 public class RecyclePage extends AppCompatActivity implements recycler_view_interface {
 
-    String []data = {""};
+    String[] data = {""};
     int counter = 0;
 
 
@@ -39,8 +39,11 @@ public class RecyclePage extends AppCompatActivity implements recycler_view_inte
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycle_page);
 
+
+        category_data_types = UserCategories.user_category_information;
+
         List<String> items = new LinkedList<>();
-       // items.add("My Category 1");
+        // items.add("My Category 1");
 
 
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
@@ -50,7 +53,7 @@ public class RecyclePage extends AppCompatActivity implements recycler_view_inte
 
         Button btn = findViewById(R.id.Add);
 
-        EditText editText= (EditText)findViewById(R.id.et_item);
+        EditText editText = (EditText) findViewById(R.id.et_item);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,21 +62,28 @@ public class RecyclePage extends AppCompatActivity implements recycler_view_inte
             }
         });
 
-        
 
         findViewById(R.id.Add).setOnClickListener(view -> {
-         //   items.add(data[counter%3]);
-        //    counter++;
-         //   items.add("Code it 2");
-           // Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_SHORT).show();"
-            items.add(editText.getText().toString());
+            //   items.add(data[counter%3]);
+            //    counter++;
+            //   items.add("Code it 2");
+            // Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_SHORT).show();"
+            // items.add(editText.getText().toString());
 
-            category_data_types.add(new Category_Data_Type(editText.getText().toString() , 0, 0, 0, null));
+            //category_data_types.add(new Category_Data_Type(editText.getText().toString() , 0, 0, 0, null));
 
+            AddButton();
 
-
-            adapter.notifyItemInserted(items.size()+1);
+            //adapter.notifyItemInserted(items.size()+1);
         });
+
+        AddItemRecycler(adapter, items);
+
+        int num_cats = category_data_types.size();
+
+        Toast.makeText(this, "hello" + num_cats, Toast.LENGTH_SHORT).show();
+
+
     }
 
 
@@ -105,4 +115,42 @@ public class RecyclePage extends AppCompatActivity implements recycler_view_inte
         startActivity(intent);
 
     }
+
+
+    void AddButton() {
+
+        Intent intent = new Intent(this, Create_Category.class);
+
+        startActivity(intent);
+
+    }
+
+    void AddItemRecycler(DemoAdapter adapter, List<String> items) {
+
+
+        //Toast.makeText(this, getIntent().getStringExtra("category_name_data") + getIntent().getIntExtra("category_max_items_data", 0) , Toast.LENGTH_SHORT).show();
+
+
+        for (Category_Data_Type cat : UserCategories.user_category_information) {
+
+            if (cat.category_name == null) {
+
+                Toast.makeText(this, "fucked", Toast.LENGTH_SHORT).show();
+            }
+
+            items.add(cat.category_name);
+
+            //Toast.makeText(this, cat.category_name, Toast.LENGTH_SHORT).show();
+
+            //category_data_types.add(cat);
+
+            adapter.notifyItemInserted(items.size() + 1);
+
+
+        }
+
+
+    }
+
+
 }
